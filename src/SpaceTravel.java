137e844426de8f8d11688105b58849c56e2efd5e
velocitypulse <velocitypulse@outlook.fr>
2017-02-15 14:55:00 +0100

    Ajout de 'h' et 'q'

--- src/SpaceTravel.java
+++ src/SpaceTravel.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by cchameyr on 2/15/17.
  */
 public class SpaceTravel {
     public static void main (String[] args)
     {
+        Scanner     scan = new Scanner(System.in);
+        String      choice;
+        boolean     quit = false;
 
+        while (!quit) {
+            choice = (scan.nextLine()).trim();
+            if (choice.length() > 0) {
+
+                switch (choice.charAt(0)) {
+                    case 'q' :
+                        quit = true;
+                        break;
+                    case 'h' :
+                        System.out.println("Print help :");
+                        break;
+                    default :
+                        System.out.println(choice.charAt(0) + " command not found\nUse 'h' for help");
+                        break;
+                }
+            }
+        }
+        System.out.println("Bye");
     }
 }
